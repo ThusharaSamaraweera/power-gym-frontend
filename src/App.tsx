d598cfa12a30b2e76425dabd4d4b8206f9076231
ThusharaSamaraweera <thusharadasun204@gmail.com>
2024-07-12 22:10:09 +0530

    setup shandcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Button variant='link' className='text-red-500'>
+        click
+      </Button>
     </>
   );
 }
