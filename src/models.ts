70e2e6c67880c3060c1128af27325840f8ea40e5
ThusharaSamaraweera <thusharadasun204@gmail.com>
2024-08-04 01:00:59 +0530

    refactor signup

--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,11 @@
+export interface IUser extends SignupUser{
+  dateOfBirth?: string;
+  gender?: string;
+  image?: string;
+  phone?: string;
+  status: UserStatus
+}
+
 export interface SignupUser {
   name: string
   email: string;
@@ -9,4 +17,10 @@ export enum UserRoles {
   ADMIN = 'ADMIN',
   TRAINER = 'TRAINER',
   MEMBER = 'MEMBER',
+}
+
+
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  DISABLED = 'DISABLED',
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import { SignupUser } from "../models";
 import { HTTPS_METHODS, restClient } from "../utils/restClient";
 
 export const signUp = async (payload: SignupUser) => {
-    const res = await restClient(HTTPS_METHODS.POST, '/users', payload)
+    const res = await restClient(HTTPS_METHODS.POST, '/auth/signup', payload)
     return res?.data
 }
 
